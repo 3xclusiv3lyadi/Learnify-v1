db8de32fb49fc6f80a58875bf5d2acefe3df1ae2
Adithya <06adithyas7@gmail.com>
2025-04-13 16:05:06 +0000

    add more bottom padding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <div className="pb-20">
+        <div className="pb-32">
           {children}
         </div>
         <PersistentNavbar />

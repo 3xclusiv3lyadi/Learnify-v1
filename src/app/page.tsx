e2281d39ea2c3874266c8f0ca920f61eed0bcca5
Adithya <06adithyas7@gmail.com>
2025-04-18 11:29:44 +0000

    add more bubbles on whole home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,6 +48,11 @@ const HomeScreen = () => {
       <div className="bubble bubble-3"></div>
       <div className="bubble bubble-4"></div>
       <div className="bubble bubble-5"></div>
+      <div className="bubble bubble-6"></div>
+      <div className="bubble bubble-7"></div>
+      <div className="bubble bubble-8"></div>
+      <div className="bubble bubble-9"></div>
+      <div className="bubble bubble-10"></div>
        {/* Top Navigation Bar */}
        <div className="w-full bg-[#48252f] text-white p-4 flex items-center justify-between rounded-md shadow-md mb-4">
         <div className="flex items-center space-x-4">
